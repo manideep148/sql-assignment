f72191538bc16abe96cbba0bb9eeb0ff1fba9e38
manideep148 <134506125+manideep148@users.noreply.github.com>
2023-07-08 13:00:14 +0530

    Add files via upload

---
+++ assignment.sql
@@ -0,0 +1,11 @@
+create database todolist;
+
+use todolist;
+
+CREATE TABLE todo_list (
+  task_id INT PRIMARY KEY,
+  task_name VARCHAR(255),
+  description VARCHAR(255),
+  is_completed BOOLEAN
+);
+
